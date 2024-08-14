a802fdf5120727d559aff5c942a8dfa8fac72105
Alexander Dryannov <l3x.d@yandex.ru>
2024-08-14 22:03:48 +0300

    Исправил закрепление постов

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,11 +1,29 @@
 from django.contrib import admin
 
-from apps.posts.models import Post
+from apps.posts.models import Post, FixedPost
+
+
+@admin.action(description='Закрепить')
+def set_fixed_post(modeladmin, request, queryset):
+    for obj in queryset:
+        FixedPost.objects.create(target=obj)
+
+
+@admin.register(FixedPost)
+class FixedPostAdmin(admin.ModelAdmin):
+    fields = ('target',)
+    list_display = (
+        'id',
+        'target',
+    )
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    fields = ['title', 'author', 'text', 'is_fixed', 'created_at', 'updated_at']
+    actions = [
+        set_fixed_post,
+    ]
+    fields = ('title', 'author', 'text', 'created_at', 'updated_at')
     list_display = ('id', 'title', 'author')
     list_display_links = ('id', 'title')
     list_filter = ('created_at', 'author')
