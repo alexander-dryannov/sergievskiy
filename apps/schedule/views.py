227b8014542e85f389b117e6ea82004b2c38aa76
Alexander Dryannov <l3x.d@yandex.ru>
2024-08-20 01:57:33 +0300

    hotfix schedule

--- apps/schedule/views.py
+++ apps/schedule/views.py
@@ -1,3 +1,5 @@
+import locale
+
 from django.views import generic
 
 from . import models
@@ -11,11 +13,23 @@ class WeekScheduleListView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['data'] = [
-            {
-                'week': {'id': week.id, 'name': week.name},
-                'schedule': list(models.Schedule.objects.filter(is_visible=True, week_id=week.id)),
-            }
-            for week in self.model.objects.filter(is_visible=True)
-        ]
+        schedule = []
+
+        for week in self.model.objects.filter(is_visible=True):
+            data = {'week': week}
+
+            for sch in models.Schedule.objects.filter(is_visible=True, week_id=week.id):
+                sch_date = sch.event.date()
+
+                if not data.get('schedule'):
+                    data['schedule'] = {sch_date: []}
+                try:
+                    data['schedule'][sch_date].append(sch)
+                except KeyError:
+                    data['schedule'][sch_date] = []
+                    data['schedule'][sch_date].append(sch)
+            schedule.append(data)
+
+        context['data'] = schedule
+
         return context
