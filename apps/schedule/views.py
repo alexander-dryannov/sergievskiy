44178d185c9c453e5b46890832f2b69d20ea5ca3
Alexander Dryannov <l3x.d@yandex.ru>
2024-09-28 15:13:10 +0300

    Показывать последние 2 седмицы

--- apps/schedule/views.py
+++ apps/schedule/views.py
@@ -6,22 +6,23 @@ from . import models
 
 
 def week_schedule(request):
+    weeks = []
     context = {}
-    week = models.Week.objects.filter(is_visible=True).last()
+    week_qs = models.Week.objects.filter(is_visible=True).order_by('-pk')[:2]
+    week_qs = reversed(week_qs)
 
-    if not week:
-        return render(request, template_name='week/list.html', context=context)
+    if week_qs:
+        for week in week_qs:
+            days = models.Day.objects.filter(is_visible=True, date__gte=date.today())
 
-    days = models.Day.objects.filter(is_visible=True, date__gte=date.today())
+            if days:
+                days_filter = days.filter(week=week)
+                data = {'week': week, 'days_and_events': []}
 
-    if not days:
-        return render(request, template_name='week/list.html', context=context)
-
-    days_filter = days.filter(week=week)
-    data = {'week': week, 'days_and_events': []}
-    for day in days_filter:
-        data['days_and_events'].append(
-            {'day': day, 'events': models.Event.objects.filter(day=day, is_visible=True)}
-        )
-    context['week'] = data
+                for day in days_filter:
+                    data['days_and_events'].append(
+                        {'day': day, 'events': models.Event.objects.filter(day=day, is_visible=True)}
+                    )
+                weeks.append(data)
+        context['weeks'] = weeks
     return render(request, template_name='week/list.html', context=context)
